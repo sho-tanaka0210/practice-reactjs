12580c63308cc23e43e70d1ea0ae4f48867d5f35
mizukichi0210 <canaryyellow.nl@gmail.com>
2020-05-04 14:37:09 +0000

    Updates

---
+++ precache-manifest.4e85df22a4b612c381eb5006d8246856.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24683965b61f985cc9e3b44b008a9679",
+    "url": "/practice-reactjs/index.html"
+  },
+  {
+    "revision": "a3737569e97fd09db102",
+    "url": "/practice-reactjs/static/css/main.8cdbcaf3.chunk.css"
+  },
+  {
+    "revision": "aaa531bc5c7924fcbc6b",
+    "url": "/practice-reactjs/static/js/2.37d657b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practice-reactjs/static/js/2.37d657b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3737569e97fd09db102",
+    "url": "/practice-reactjs/static/js/main.0aafa98f.chunk.js"
+  },
+  {
+    "revision": "12b6c8382996e8295cc8",
+    "url": "/practice-reactjs/static/js/runtime-main.5ca6bb17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practice-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
