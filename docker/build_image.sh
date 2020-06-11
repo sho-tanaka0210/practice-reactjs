c2c848201048bf3e3e4fa4239b379f4b0e59eae8
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-06-11 21:04:07 +0900

    Modify build_image.sh

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -eu
 
+IMAGE_NAME='practice-reactjs:latest'
+
+# 既存のlatestイメージが存在する場合、latestイメージを削除する
+if [ "$(docker image ls -q ${IMAGE_NAME})" ]; then
+  docker rmi practice-reactjs:latest
+fi
+
 cp -p package.json ./docker/
 docker build ./docker/. -t practice-reactjs
