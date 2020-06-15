f06742959ac754e11eaead3de0c608ebafb382d8
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-06-16 00:48:43 +0900

    Remove unused imports

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,16 +8,10 @@ import {
   Divider,
   Grid,
   Header,
-  Icon,
   Image,
   List,
   Segment,
 } from 'semantic-ui-react'
-
-/* eslint-disable react/no-multi-comp */
-/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
 
 const ResponsiveContainer = ({ children, mobile, homepageHeading }) => (
   <div>
