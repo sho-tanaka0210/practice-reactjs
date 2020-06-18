ee22c2fb665a67e91c5cd7f309992650e9c5e532
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-06-18 21:44:03 +0900

    homeヘッダー改変 unittest

---
+++ src/components/header/homepageHeading.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import HomepageHeading from './homepageHeading';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('h1');
+  // container *must* be attached to document so events work correctly.
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('View homepage header', () => {
+  act(() => {
+    render(<HomepageHeading />, container);
+
+    const homepageheading = container.querySelector('.ui.inverted.header');
+    expect(homepageheading.textContent).toBe('リポジトリ概要紹介');
+  });
+});
