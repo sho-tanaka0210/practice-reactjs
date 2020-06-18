b5e887ad0a85fcf245aceb648baae346aaabea7a
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-06-18 21:41:21 +0900

    homeのヘッダー改変

--- src/components/header/homepageHeading.jsx
+++ src/components/header/homepageHeading.jsx
@@ -1,10 +1,8 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import {
-  Button,
   Container,
   Header,
-  Icon,
 } from 'semantic-ui-react'
 
 class HomepageHeading extends Component {
@@ -13,29 +11,15 @@ class HomepageHeading extends Component {
     return <Container text>
     <Header
       as='h1'
-      content='Imagine-a-Company'
+      content='リポジトリ概要紹介'
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
         fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
+        marginTop: mobile ? '0em' : '1em',
+        marginBottom: '1em',
       }}
     />
-    <Header
-      as='h2'
-      content='Do whatever you want when you want to.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>
   </Container>
   }
 
