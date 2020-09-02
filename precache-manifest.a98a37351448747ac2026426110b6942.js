cbf1c6d05fd8fc886e11cf8eb87196136d4987e9
Mizukichi0210 <Mizukichi0210@users.noreply.github.com>
2020-09-02 10:21:10 +0000

    deploy: b8be68af77982d808c133058202c1e5eb421551f

--- precache-manifest.a98a37351448747ac2026426110b6942.js
+++ precache-manifest.a98a37351448747ac2026426110b6942.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1848430c9b42f06ac2e56b453aed2bbb",
+    "revision": "05d3937203a0ac2ba562ac495228d420",
     "url": "/practice-reactjs/index.html"
   },
   {
-    "revision": "63f11e35e27abab0a3ce",
+    "revision": "acf2423dc322aa0b65cb",
     "url": "/practice-reactjs/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "648827b4c4360b771b3e",
-    "url": "/practice-reactjs/static/js/2.78b6d783.chunk.js"
+    "revision": "5f773e40bf5858f4aca4",
+    "url": "/practice-reactjs/static/js/2.63fd0c2d.chunk.js"
   },
   {
     "revision": "4d47ffde3dc69784e8e23fd910817ef8",
-    "url": "/practice-reactjs/static/js/2.78b6d783.chunk.js.LICENSE.txt"
+    "url": "/practice-reactjs/static/js/2.63fd0c2d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "63f11e35e27abab0a3ce",
-    "url": "/practice-reactjs/static/js/main.4dc1b541.chunk.js"
+    "revision": "acf2423dc322aa0b65cb",
+    "url": "/practice-reactjs/static/js/main.8bd2f1eb.chunk.js"
   },
   {
     "revision": "0a02858b9fd343122485",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/practice-reactjs/precache-manifest.b9c50f89078c2b98f053c83512e77796.js"
+  "/practice-reactjs/precache-manifest.a98a37351448747ac2026426110b6942.js"
 );
 
 self.addEventListener('message', (event) => {
