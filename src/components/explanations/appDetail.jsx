73dc03d48e52432416c78f765188de2b0b6e5d35
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-09-06 20:27:27 +0900

    .

---
+++ src/components/explanations/appDetail.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ViewDetailButton from '../buttons/viewDetailButton';
+import {
+  Responsive,
+} from 'semantic-ui-react'
+
+class AppDetail extends Component {
+  state = {}
+
+  render() {
+    return <ViewDetailButton></ViewDetailButton>;
+  }
+}
+
+export default AppDetail;
