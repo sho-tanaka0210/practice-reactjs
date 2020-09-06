c1b4486ff202ae0a7a64fcbe4c353e1fb6b5ae61
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-09-07 00:10:16 +0900

    overview修正

--- src/components/explanations/overview.jsx
+++ src/components/explanations/overview.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
+import {
+  Grid,
+  Image,
+} from 'semantic-ui-react';
+
 
 class Overview extends Component {
   state = {}
 
   render() {
     const { overview } = this.props
+    const { imageSrc1 } = this.props;
+    const { imageSrc2 } = this.props;
 
     return(
-      <p style={{ fontSize: '1.33em' }}>
-        {overview}
-      </p>
+      <div>
+        <p style={{ fontSize: '1.33em' }}>
+          {overview}
+        </p>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Image src={imageSrc1}></Image>
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <Image src={imageSrc2}></Image>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
