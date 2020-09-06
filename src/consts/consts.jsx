02e1b958d723eda9e33ad454054787754df0a455
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-09-07 00:10:31 +0900

    定数追加

--- src/consts/consts.jsx
+++ src/consts/consts.jsx
@@ -1,6 +1,6 @@
 const Consts = {
   DISCORD_APP_NAME: 'DiscordのVC入退室通知ツール',
-  DISCORD_APP_OVERVIEW: 'このBOTを導入したDiscordサーバのボイスチャットへIN or OUTした際のユーザ名を指定のチャンネルへ投稿します。',
+  DISCORD_APP_OVERVIEW: 'このBOTを導入したDiscordサーバのボイスチャットへIN or OUTした際のユーザ名を指定のチャンネルへ投稿する',
   DISCORD_APP_MOTION_CAPUTURE: 'https://camo.githubusercontent.com/4677d97b0e5fa3dbd57f4d6547890ba11090a6ac/68747470733a2f2f7261772e6769746875622e636f6d2f77696b692f6d697a756b69636869303231302f4d616e6167652d456e74657265642d616e642d4c6566742d6f6e2d446973636f72642f6578616d706c655f6769662e676966',
   DISCORD_APP_OPERATION_SETTING_CAPTURE: 'https://user-images.githubusercontent.com/37664176/83553813-c0d99480-a546-11ea-92d6-30500db4eddd.png',
 
@@ -9,8 +9,13 @@ const Consts = {
   CHECK_RESPONSE_OVERVIEW_CAPTURE: 'https://user-images.githubusercontent.com/37664176/73596611-e5a50780-4566-11ea-8fc3-7ff75e492673.png',
   CHECK_RESPONSE_OPERATION_CAPTURE: 'https://user-images.githubusercontent.com/37664176/79860246-46790900-840d-11ea-8429-05551ffc3936.png',
 
+  DOCKER_IMAGE_NAME: 'Dockerイメージ自動更新・デプロイ',
+  DOCKER_IMAGE_OVERVIEW: '定期的なDockerイメージの更新を自動化させる',
+  DOCKER_IMAGE_WORKING_CAPTURE: 'https://user-images.githubusercontent.com/37664176/91982180-d25c5380-ed64-11ea-8e1f-a3a1481f71de.png',
+
   BUTTON_READ_MORE: 'Read more...',
   BUTTON_CLOSE: 'Close',
 }
+
 
 export default Consts;
