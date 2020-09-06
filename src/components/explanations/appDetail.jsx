bccd718c77bf8bff1665d11938f656e40072f986
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-09-07 00:09:55 +0900

    .

--- src/components/explanations/appDetail.jsx
+++ src/components/explanations/appDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ViewDetailButton from '../buttons/viewDetailButton';
+import AppEnum from '../../consts/appEnum';
 import {
   Responsive,
 } from 'semantic-ui-react'
@@ -7,8 +8,16 @@ import {
 class AppDetail extends Component {
   state = {}
 
+  viewDetail = () => {
+    this.setState({ isViewDetails: !this.state.isViewDetails});
+  }
+
+  getDetailText = () => {
+    return <div>テスト用</div>;
+  }
+
   render() {
-    return <ViewDetailButton></ViewDetailButton>;
+    return <ViewDetailButton detail={this.getDetailText()}></ViewDetailButton>;
   }
 }
 
